5e45b49941fe045f1972547600d2ffd2725657d4
marcosjr12132 <130661526+marcosjr12132@users.noreply.github.com>
2023-11-19 12:14:28 -0300

    Update main.php

--- main.php
+++ main.php
@@ -1,153 +1,341 @@
-<?php
-session_start(); // Inicia a sessão
-
-include('conexao.php'); // Arquivo de conexão com o banco de dados
-
-//if (isset($_POST['email']) && isset($_POST['senha'])) { // Verifica se os campos 'email' e 'senha' foram enviados via POST
-
-   // $email = $mysqli->real_escape_string($_POST['email']); 
-    //$senha = $mysqli->real_escape_string($_POST['senha']); // Proteção contra SQL injection
-
-    //if (empty($email)) { // Verifica se o campo 'email' está vazio
-        //echo "Preencha seu e-mail";
-    //} elseif (empty($senha)) { // Verifica se o campo 'senha' está vazio
-        //echo "Preencha sua senha";
-    //} else {
-        //$sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'"; // Monta a consulta SQL para verificar as credenciais
-        //$sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error); // Executa a consulta SQL
-
-        //$quantidade = $sql_query->num_rows; // Obtém o número de linhas retornadas pela consulta
-
-        //if ($quantidade == 1) { // Se uma linha for retornada, o login é bem-sucedido
-
-            //$usuario = $sql_query->fetch_assoc(); // Obtém os dados do usuário do resultado da consulta
-
-            //$_SESSION['id'] = $usuario['id']; // Define a variável de sessão 'id'
-            //$_SESSION['nome'] = $usuario['nome']; // Define a variável de sessão 'nome'
-
-            // Agora você tem as informações do usuário nas variáveis de sessão,
-            // mas não precisa exibi-las diretamente na página.
-
-            //header("Location: usercadastrado.php");
-            //exit(); // Adicionado para evitar que o restante do código seja executado após a redireção
-
-        //} else {
-            //cho "Falha ao logar! E-mail ou senha incorretos"; // Exibe uma mensagem de erro se o login falhar
-        //}
-    //}
-//}
-?>
-
-
-<html>
+<!DOCTYPE html>
+<html lang="pt">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="main.css">
-    <title>Rota cultural</title>
-    <script src="main.js" defer></script>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cesta Cultural</title>
+    <style>
+        p {
+            text-align: center;
+        }
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
 
+        header {
+            background-color: #FFB535;
+            padding: 20px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        header h1 {
+            color: #f5f5f5;
+            margin: 0;
+        }
+
+        nav ul {
+            list-style-type: none;
+            padding: 0;
+            display: flex;
+        }
+
+        nav ul li {
+            margin-left: 20px;
+        }
+
+        nav ul li a {
+            color: #f5f5f5;
+            text-decoration: none;
+            padding: 10px;
+            border-radius: 5px;
+            transition: background-color 0.3s ease;
+        }
+
+        nav ul li a:hover {
+            background-color: #BB7804;
+        }
+
+        main {
+            padding: 20px;
+        }
+
+        main h2 {
+            margin-bottom: 20px;
+        }
+
+        .cta-buttons {
+            display: flex;
+            justify-content: space-around;
+            margin: 20px 0;
+        }
+
+        .cta-buttons a {
+            color: #fff;
+            text-decoration: none;
+            padding: 15px 25px;
+            border-radius: 25px;
+            font-weight: bold;
+            transition: background-color 0.3s ease;
+        }
+
+        .cta-buttons a.login {
+            background-color: #2ecc71;
+        }
+
+        .cta-buttons a.signup {
+            background-color: #3498db;
+        }
+
+        .cta-buttons a:hover {
+            background-color: #485460;
+        }
+
+        footer {
+            background-color: #FFB535;
+            color: #f5f5f5;
+            padding: 20px;
+            text-align: center;
+        }
+
+        /* Popup Styling */
+        .popup {
+            display: none;
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            padding: 20px;
+            background-color: #fff;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            z-index: 1;
+        }
+
+        .overlay {
+            display: none;
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: rgba(0, 0, 0, 0.5);
+            z-index: 0;
+        }
+        
+        .image-container {
+        text-align: center;
+    }
+
+    .image-container img {
+        width: 100%;
+        max-width: 100%;
+        height: auto;
+        border-radius: 8px; /* Optional: Add border-radius for rounded corners */
+        margin-top: 20px; /* Adjust the margin as needed */
+    }
+    
+    .main-button {
+            display: block;
+            margin: 20px auto;
+            padding: 15px 25px;
+            background-color: #E40060; /* Cor do botão de inscrições */
+            color: #fff;
+            text-decoration: none;
+            border-radius: 25px;
+            font-weight: bold;
+            transition: background-color 0.3s ease;
+            text-align: center;
+        }
+
+        .main-button:hover {
+            background-color: #AF024B; /* Cor no hover similar ao botão de inscrições */
+        }
+        
+         button.close-button {
+            display: block;
+            width: 100%;
+            padding: 10px;
+            background-color: #FFA813;
+            color: white;
+            border: none;
+            cursor: pointer;
+            margin-top: 20px;
+            transition: background-color 0.3s;
+        }
+
+        button.close-button:hover {
+            background-color: #BB7804;
+        }
+        .overlay {
+            display: none;
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: rgba(0, 0, 0, 0.5);
+            z-index: 1;
+        }
+
+        .login-popup,
+        .registration-popup {
+            display: none;
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            padding: 20px;
+            background-color: #fff;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            z-index: 2;
+        }
+
+        .close-popup-button {
+            position: absolute;
+            top: 10px;
+            right: 10px;
+            font-size: 20px;
+            background: none;
+            border: none;
+            cursor: pointer;
+            background-color: black;
+            color: white;
+        }
+    </style>
 </head>
 <body>
-   
-    <div class="container">
-    <div class="button-container">
-        <a onclick="openRegistrationPopup()" class="button">Cadastro</a>
-        <a onclick="openLoginPopup()" class="button">Login</a>
-        
-    </div>
-        <h1>Bem-vindo ao Rota Cultural</h1>
-        <p>Descubra novos horizontes artísticos com a Rota Cultural: o programa escolar que leva você a uma viagem pelo conhecimento.</p>
+    <header>
+        <h1>Cesta Cultural</h1>
+        <nav>
+            <ul>
+                <li><a href="#" onclick="showPopup()">Sobre</a></li>
+                <li><a href="paginadeimagens.php">Mural</a></li>
+                <li><a href="#" onclick="showLoginPopup()">Login</a></li>
+                <li><a href="#" onclick="showCadastroPopup()">Cadastro</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <p>Descubra novos horizontes e experiências, participe de um evento da Cesta Cultural!</p>
         <div class="image-container">
-            <img src="https://static5.vvale.com.br/wp-content/uploads/2017/03/DSC09145.jpg" alt="Imagem">
+            <img src="img1.jpeg" alt="Horizontal Image">
         </div>
-        <h2>Faça seu cadastro e garanta sua participação em todos os eventos.</h2>
-</div>
-  <div class="card">
-    <h3>Evento 1</h3>
-    <p>Data: 15 de junho de 2023</p>
-    <p>Descrição: Descrição do Evento 1.</p>
-  </div>
-  
-  <div class="card">
-    <h3>Evento 2</h3>
-    <p>Data: 22 de junho de 2023</p>
-    <p>Descrição: Descrição do Evento 2.</p>
-  </div>
-  
-<!-- Adicione isso ao seu HTML onde você deseja exibir o formulário de depoimento -->
-<div class="depoimento-form">
-    <h2>Deixe seu Depoimento</h2>
-    <form action="processa_depoimento.php" method="POST" enctype="multipart/form-data">
-        <?php
-        // Verifica se o usuário está logado
-        if (isset($_SESSION['nome']) && isset($_SESSION['caminho_perfil'])) {
-            // Se estiver logado, exibe a foto de perfil e nome do usuário
-            echo '<img src="' . $_SESSION['caminho_perfil'] . '" alt="Foto de Perfil">';
-            echo '<input type="hidden" name="nome" value="' . $_SESSION['nome'] . '">';
-            echo '<input type="hidden" name="caminho_perfil" value="' . $_SESSION['caminho_perfil'] . '">';
-        }
-        ?>
+        <a href="incricoes.html" class="main-button">Participe Agora</a>
+    </main>
+    <footer>
+        <p>&copy; 2022 - Cesta Cultural. Todos os direitos reservados.</p>
+    </footer>
 
-        <label>Depoimento</label>
-        <textarea name="depoimento" rows="4" required></textarea>
+    <div class="overlay" onclick="hidePopup()"></div>
+    <div class="popup" id="popup">
+        <p>Bem-vindo à Rota Cultural: Sua Plataforma de Organização de Eventos Escolares no CEFET-MG!
+            Na Rota Cultural, acreditamos no poder transformador dos eventos escolares, e é por isso que criamos uma plataforma dedicada a simplificar e aprimorar a organização dessas experiências educativas. Seja você um educador, aluno ou membro da comunidade escolar, a Rota Cultural está aqui para ajudar a tornar seus eventos inesquecíveis.</p>
+        <button class="close-button" onclick="hidePopup()">Fechar</button>
+    </div>
 
-        <button type="submit">Enviar Depoimento</button>
-    </form>
-</div>
+    <div class="overlay" onclick="closeLoginPopup(); closeCadastroPopup();"></div>
 
-
-    <button class="help-button" onclick="openPopup()">Sobre o site</button>
-
-        <div class="popup" id="popup">
-            <div class="popup-content">
-                <button class="close-button" onclick="closePopup()">&#x2716;</button>
-                <h2>Bem-vindo ao Rota Cultural</h2>
-                <p>O seu portal de referência para a organização de eventos culturais nas escolas. Aqui, você encontrará recursos e ferramentas para planejar e realizar eventos culturais nas escolas, incluindo apresentações de teatro, concertos, exposições de arte e muito mais. Estamos empenhados em tornar a experiência cultural nas escolas mais acessível e inspiradora para todos os envolvidos. Juntos, podemos trazer a magia da cultura para as escolas e enriquecer a jornada educacional dos estudantes.</p>
-            </div>
-        </div>
-    
-
-    <div class="registration-popup">
-        <button class="close-button" onclick="closeRegistrationPopup()">&#x2716;</button>
-        <div class="registration-popup-content">
-            <h2>Cadastro</h2>
-            <p>Insira seus dados para se cadastrar:</p>
-    <form action="processa.php" method="POST" onsubmit="return validateRegistrationForm()">
-    <label>Nome</label><input type="text" name="nome" required>
-    <label>E-mail</label><input type="email" name="email" required>
-    <label>Telefone</label><input type="tel" id="phone" name="phone" placeholder="Telefone" maxlength="15" pattern="\(\d{2}\) \d{4,5}-\d{4}" oninput="this.value = this.value.replace(/[^0-9() -]/g, '')" onkeypress="formatPhoneNumber(event)" required>
-    <label>Nova Senha</label><input type="password" id="password" name="senha" maxlength="10" placeholder="Nova senha" required>
-    <i class="password-icon fa fa-eye" onclick="togglePasswordVisibility('password')"></i>
-    <label>Confirme a Senha</label><input type="password" id="confirm-password" name="confirm-password" maxlength="10" placeholder="Confirme a senha" required>
-    <i class="password-icon fa fa-eye" onclick="togglePasswordVisibility('confirm-password')"></i>
-    <label>Tipo de Cadastro</label>
-    <input type="radio" name="tipo" value="editor" required> Cadastrar como Editor<br>
-    <input type="radio" name="tipo" value="visitante" required> Cadastrar como Visitante<br>
-    <button type="submit">Cadastrar</button>
-    </form>
-
-            </form>
-            <p>Já possui uma conta? <a onclick="switchToLoginPopup()">Faça login</a></p>
-        </div>
-        </div>
-        
-    <div class="login-popup">
-     <!--   <button class="close-button" onclick="closeLoginPopup()">&#x2716;</button> -->
+    <div class="login-popup" id="loginPopup">
+        <button class="close-popup-button" onclick="closeLoginPopup()">&#x2716;</button>
         <div class="login-popup-content">
             <h2>Login</h2>
             <p>Insira seus dados para fazer login:</p>
             <form action="login.php" method="POST">
-    <label>E-mail</label><input type="text" name="email">
-    <label>Senha</label><input type="password" name="senha">
-    <button type="submit">Login</button>
-</form>
-            <p>Não possui uma conta? <a onclick="switchToRegistrationPopup()">Cadastra-se</a></p>
+                <label>E-mail</label><input type="text" name="email">
+                <label>Senha</label><input type="password" name="senha">
+                <button type="submit">Login</button>
+            </form>
+            <p>Não possui uma conta? <a href="#" onclick="showCadastroPopup()">Cadastrar-se</a></p>
         </div>
+    </div>
+
+    <div class="registration-popup" id="cadastroPopup">
+        <button class="close-popup-button" onclick="closeCadastroPopup()">&#x2716;</button>
+        <div class="registration-popup-content">
+            <h2>Cadastro</h2>
+            <p>Insira seus dados para se cadastrar:</p>
+            <form action="processa.php" method="POST" onsubmit="return validateRegistrationForm()">
+                <label>Nome</label><input type="text" name="nome" required>
+                <label>E-mail</label><input type="email" name="email" required>
+                <label>Telefone</label><input type="tel" id="phone" name="phone" placeholder="Telefone" maxlength="15" pattern="\(\d{2}\) \d{4,5}-\d{4}" oninput="this.value = this.value.replace(/[^0-9() -]/g, '')" onkeypress="formatPhoneNumber(event)" required>
+                <label>Nova Senha</label><input type="password" id="password" name="senha" maxlength="10" placeholder="Nova senha" required>
+                <i class="password-icon fa fa-eye" onclick="togglePasswordVisibility('password')"></i>
+                <label>Confirme a Senha</label><input type="password" id="confirm-password" name="confirm-password" maxlength="10" placeholder="Confirme a senha" required>
+                <i class="password-icon fa fa-eye" onclick="togglePasswordVisibility('confirm-password')"></i>
+                <label>Tipo de Cadastro</label>
+                <input type="radio" name="tipo" value="editor" required> Cadastrar como Editor<br>
+                <input type="radio" name="tipo" value="visitante" required> Cadastrar como Visitante<br>
+                <button type="submit">Cadastrar</button>
+            </form>
+            <p>Já possui uma conta? <a href="#" onclick="showLoginPopup()">Faça login</a></p>
         </div>
-    
-	<footer>
-		<p>&copy; 2023 Rota Cultural </p> <!-- nome criação e selo do site -->
-	</footer>
+    </div>
+
+
+    <script>
+        function showPopup() {
+            document.getElementById('popup').style.display = 'block';
+            document.querySelector('.overlay').style.display = 'block';
+        }
+
+        function hidePopup() {
+            document.getElementById('popup').style.display = 'none';
+            document.querySelector('.overlay').style.display = 'none';
+        }
+
+        function showLoginPopup() {
+            document.getElementById('loginPopup').style.display = 'block';
+            document.querySelector('.overlay').style.display = 'block';
+        }
+
+        function closeLoginPopup() {
+            document.getElementById('loginPopup').style.display = 'none';
+            document.querySelector('.overlay').style.display = 'none';
+        }
+
+        function showCadastroPopup() {
+            document.getElementById('cadastroPopup').style.display = 'block';
+            document.querySelector('.overlay').style.display = 'block';
+        }
+
+        function closeCadastroPopup() {
+            document.getElementById('cadastroPopup').style.display = 'none';
+            document.querySelector('.overlay').style.display = 'none';
+        }
+    </script>
 </body>
 </html>
+
+
+  <!--  <div class="registration-popup">
+        <button class="close-popup-button" onclick="closeRegistrationPopup()">&#x2716;</button>
+        <div class="registration-popup-content">
+            <h2>Cadastro</h2>
+            <p>Insira seus dados para se cadastrar:</p>
+            <form action="processa.php" method="POST" onsubmit="return validateRegistrationForm()">
+                <label>Nome</label><input type="text" name="nome" required>
+                <label>E-mail</label><input type="email" name="email" required>
+                <label>Telefone</label><input type="tel" id="phone" name="phone" placeholder="Telefone" maxlength="15" pattern="\(\d{2}\) \d{4,5}-\d{4}" oninput="this.value = this.value.replace(/[^0-9() -]/g, '')" onkeypress="formatPhoneNumber(event)" required>
+                <label>Nova Senha</label><input type="password" id="password" name="senha" maxlength="10" placeholder="Nova senha" required>
+                <i class="password-icon fa fa-eye" onclick="togglePasswordVisibility('password')"></i>
+                <label>Confirme a Senha</label><input type="password" id="confirm-password" name="confirm-password" maxlength="10" placeholder="Confirme a senha" required>
+                <i class="password-icon fa fa-eye" onclick="togglePasswordVisibility('confirm-password')"></i>
+                <label>Tipo de Cadastro</label>
+                <input type="radio" name="tipo" value="editor" required> Cadastrar como Editor<br>
+                <input type="radio" name="tipo" value="visitante" required> Cadastrar como Visitante<br>
+                <button type="submit">Cadastrar</button>
+            </form>
+            <p>Já possui uma conta? <a onclick="switchToLoginPopup()">Faça login</a></p>
+        </div>
+    </div>
+    -->
+  <!--  <div class="login-popup">
+        <div class="login-popup-content">
+            <button class="close-popup-button" onclick="closeLoginPopup()">&#x2716;</button>
+            <h2>Login</h2>
+            <p>Insira seus dados para fazer login:</p>
+            <form action="login.php" method="POST">
+                <label>E-mail</label><input type="text" name="email">
+                <label>Senha</label><input type="password" name="senha">
+                <button type="submit">Login</button>
+            </form>
+            <p>Não possui uma conta? <a href="cadastro.php">Cadastrar-se</a></p>
+        </div>
+    </div>
+    -->
+   
