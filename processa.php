08f1054acea3e54900157a27ed33bf49bd0614ea
marcosjr12132 <130661526+marcosjr12132@users.noreply.github.com>
2023-11-19 12:13:38 -0300

    Update processa.php

--- processa.php
+++ processa.php
@@ -1,31 +1,28 @@
 <?php
 include('conexao.php');
 
-// Validar e sanitizar dados do formulário
 $nome = mysqli_real_escape_string($conn, $_POST['nome']);
 $email = mysqli_real_escape_string($conn, $_POST['email']);
 $telefone = mysqli_real_escape_string($conn, $_POST['phone']);
 $senha = mysqli_real_escape_string($conn, $_POST['senha']);
 $tipo = mysqli_real_escape_string($conn, $_POST['tipo']);
 
-// Hash da senha (use uma função mais segura em um ambiente de produção)
-$hashedSenha = password_hash($senha, PASSWORD_DEFAULT);
+// Não criptografe a senha
+//$hashedSenha = password_hash($senha, PASSWORD_DEFAULT);
+$caminhoImagem = 'imagens/profile.jpg';
+$localizacaoPadrao = 'Divinopolis, Minas Gerais';
+$idadePadrao = 0;
+$biografiaPadrao = '';
 
-// Preparar a declaração SQL
-$sql = $conn->prepare("INSERT INTO usuarios (nome, email, telefone, senha, tipo) VALUES (?, ?, ?, ?, ?)");
+$sql = $conn->prepare("INSERT INTO usuarios (nome, email, telefone, senha, tipo, caminho_imagem, localizacao, idade, biografia) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+$sql->bind_param("ssssssssi", $nome, $email, $telefone, $senha, $tipo, $caminhoImagem, $localizacaoPadrao, $idadePadrao, $biografiaPadrao);
 
-// Vincular parâmetros
-$sql->bind_param("sssss", $nome, $email, $telefone, $hashedSenha, $tipo);
-
-// Executar a declaração
 if ($sql->execute()) {
-    // Redirecionar para a página usercadastrado.php
     header("Location: usercadastrado.php");
     exit();
 } else {
     echo "Erro ao cadastrar. Por favor, tente novamente.";
 }
-
-// Fechar a conexão
 $conn->close();
 ?>
+
